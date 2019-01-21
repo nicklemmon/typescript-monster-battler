2e1c5c7a72135f273157b5b0e9f19a1ea9b3ef00
Nick Lemmon <nick.lemmon@gmail.com>
2019-01-20 23:01:15 -0500

    Add additional test

--- src/rollDie.test.ts
+++ src/rollDie.test.ts
@@ -6,3 +6,10 @@ test( 'rollDie() returns a number between 1 and 6', () => {
   expect( value ).toBeGreaterThanOrEqual( 1 );
   expect( value ).toBeLessThanOrEqual( 6 );
 });
+
+test( 'rollDie(10) returns a number between 1 and 10', () => {
+  const value = rollDie();
+
+  expect( value ).toBeGreaterThanOrEqual( 1 );
+  expect( value ).toBeLessThanOrEqual( 10 );
+});
