344114273f4028a675c7c063868991b991964ecd
Nick Lemmon <nick.lemmon@gmail.com>
2019-01-26 12:03:24 -0500

    Add typedoc instead of JSDoc

--- src/attack.ts
+++ src/attack.ts
@@ -1,6 +1,9 @@
 import Entity from './Entity';
 import rollDice from './rollDice';
 
+/**
+ * @param entity Accepts an Entity, i.e., a character. Determines attack value based on the entity's stats and simulated dice rolling.
+ */
 export default function attack( entity: Entity ) {
   const attackValue = rollDice( entity.dexterity, entity.strength );
 
